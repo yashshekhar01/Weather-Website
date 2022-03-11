5396ba13041d75aff3b8840d17436bd8ca55fc05
yashshekhar01 <yashshekhar2934@gmail.com>
2022-03-11 19:05:12 +0530

    callback abstraction II

--- app.js
+++ app.js
@@ -1,37 +1,13 @@
-const request = require('request')
 const geocode = require('./utils/geocode')
-
-// const url = 'http://api.weatherstack.com/current?access_key=03ed1cd929834798f4a12e26bf467261&query=37.8267,-122.4233&units=f'
-
-// request({ url: url, json: true }, (error, response) => {
-//         if (error) {
-//             console.log('Unable to connect to weather service!')
-//         } else if (response.body.error) {
-//             console.log('Unable to find location')
-//         } else {
-//             console.log(response.body.current.weather_descriptions[0] + ". It is currently " + response.body.current.temperature + " degress out.")
-//         }
-//     })
-
-// // Geocoding
-// // Address -> Lat/Long -> Weather
-
-// const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoieWFzaHNoZWtoYXIwMyIsImEiOiJjbDBsYmJmd3QwdW55M2t1b2g0Mmp5czZ1In0.Um0kVron1NmHUuhnFXhOAA&limit=1'
-
-// request({ url: geocodeURL, json: true }, (error, response) => {
-//     if (error) {
-//         console.log('Unable to connect to location services!')
-//     } else if (response.body.features.length === 0) {
-//         console.log('Unable to find location. Try another search.')
-//     } else {
-//         const latitude = response.body.features[0].center[1]
-//         const longitude = response.body.features[0].center[0]
-//         console.log(latitude, longitude)
-//     }
-// })
+const forecast = require('./utils/forecast')
 
 
 geocode('Boston', (error, data) => {
+    console.log('Error', error)
+    console.log('Data', data)
+})
+
+forecast(44.1545, -75.7088, (error, data) => {
     console.log('Error', error)
     console.log('Data', data)
 })
---
+++ utils/forecast.js
@@ -0,0 +1,17 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=03ed1cd929834798f4a12e26bf467261&query=' + latitude + ',' + longitude + '&units=f'
+
+    request({ url: url, json: true }, (error, response) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined)
+        } else if (response.body.error) {
+            callback('Unable to find location', undefined)
+        } else {
+            callback(undefined, response.body.current.weather_descriptions[0] + '. It is currently ' + response.body.current.temperature + ' degress out.')
+        }
+    })
+}
+
+module.exports = forecast
