2b16d2645bec7430969ace2e8b996d5933c93923
yashshekhar01 <yashshekhar2934@gmail.com>
2022-03-15 01:08:48 +0530

    Add high and low to forecast

--- utils/forecast.js
+++ utils/forecast.js
@@ -9,9 +9,9 @@ const forecast = (latitude, longitude, callback) => {
         } else if (response.body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, response.body.current.weather_descriptions[0] + '. It is currently ' + response.body.current.temperature + ' degress out.')
+            callback(undefined, response.body.current.weather_descriptions[0] + '. It is currently ' + response.body.current.temperature + ' degress out. It feels like ' + response.body.current.feelslike + ' degrees out. The humidity is ' + response.body.current.humidity + "%.")
         }
     })
-}
+} 
 
 module.exports = forecast
