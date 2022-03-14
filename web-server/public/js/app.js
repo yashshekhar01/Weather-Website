7cf9b5ae05eb6d4ab09f90e8eae89748fe3b097b
yashshekhar01 <yashshekhar2934@gmail.com>
2022-03-14 18:40:01 +0530

    Creating a search form

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,6 +1,13 @@
 console.log('Client side javascript file is loaded!')
 
-fetch('http://localhost:3000/weather?address=boston').then((response) => {
+const weatherform = document.querySelector('form')
+const search = document.querySelector('input')
+
+weatherform.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const location = search.value
+
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
     response.json().then((data) => {
         if(data.error) {
             console.log(data.error)
@@ -9,4 +16,5 @@ fetch('http://localhost:3000/weather?address=boston').then((response) => {
             console.log(data.forecast)
         }
     })
+  })
 })
